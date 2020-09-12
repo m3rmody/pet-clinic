3d7c44d4a09dea56b56e9f90bd9fdc9d298240be
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-12 14:54:42 +0200

    Initial Project Add

---
+++ src/test/java/mermody/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mermody.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
