976b0158603dc829e0cfbd04357f01272cf3e6a4
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-14 12:25:42 +0200

    Refactored Map Impl to abstract ID generation. Closes #23.

--- pet-clinic-web/src/main/java/mermody/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mermody/springframework/petclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Dima");
         owner1.setLastName("Shevchenko");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Dima");
         owner2.setLastName("Balev");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners ..... ");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Vova");
         vet1.setLastName("Kozlow");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Kolia");
         vet2.setLastName("Popov");
 
