b211a6982adceb8ef7c92768e3d352cbcb0ac2ed
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-15 09:53:47 +0200

    Added Specialty and Associated to Vet. Closes #27.

--- pet-clinic-data/src/main/java/mermody/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mermody/springframework/petclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package mermody.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialtys;
+
+    public Set<Specialty> getSpecialtys() {
+        return specialtys;
+    }
+
+    public void setSpecialtys(Set<Specialty> specialtys) {
+        this.specialtys = specialtys;
+    }
 }
