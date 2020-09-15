079772ba95e33f5d3ffd69975c00e99667b1e26f
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-15 10:43:23 +0200

    Added Pets and Contact info to owners on stratup. Closes #33.

--- pet-clinic-data/src/main/java/mermody/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mermody/springframework/petclinic/model/Vet.java
@@ -1,10 +1,11 @@
 package mermody.springframework.petclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
-    private Set<Specialty> specialtys;
+    private Set<Specialty> specialtys = new HashSet<>();
 
     public Set<Specialty> getSpecialtys() {
         return specialtys;
--- pet-clinic-web/src/main/java/mermody/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mermody/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package mermody.springframework.petclinic.bootstrap;
 
 import mermody.springframework.petclinic.model.Owner;
+import mermody.springframework.petclinic.model.Pet;
 import mermody.springframework.petclinic.model.PetType;
 import mermody.springframework.petclinic.model.Vet;
 import mermody.springframework.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import mermody.springframework.petclinic.services.PetTypeService;
 import mermody.springframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Dima");
         owner1.setLastName("Shevchenko");
+        owner1.setAddress("123 Brikerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123-123-456");
+
+        Pet dimaslPet1 = new Pet();
+        dimaslPet1.setPetType(saveCatPetType);
+        dimaslPet1.setOwner(owner1);
+        dimaslPet1.setBirthDate(LocalDate.now());
+        dimaslPet1.setName("Barsik");
+        owner1.getPets().add(dimaslPet1);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Dima");
         owner2.setLastName("Balev");
+        owner2.setAddress("Dzerrzynski 33");
+        owner2.setCity("Pervomajsk");
+        owner2.setTelephone("777-777-777");
+
+        Pet dimaslPet = new Pet();
+        dimaslPet.setPetType(saveDogPetType);
+        dimaslPet.setOwner(owner2);
+        dimaslPet.setBirthDate(LocalDate.now());
+        dimaslPet.setName("Rosco");
+        owner2.getPets().add(dimaslPet);
 
         ownerService.save(owner2);
 
