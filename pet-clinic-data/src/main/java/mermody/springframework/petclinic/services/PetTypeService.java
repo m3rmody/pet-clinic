4bbd1c283eafd3ffc1ba88515466419adb51619f
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-15 10:09:01 +0200

    Added PetTypeMapService. Closes #29.

---
+++ pet-clinic-data/src/main/java/mermody/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package mermody.springframework.petclinic.services;
+
+import mermody.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
