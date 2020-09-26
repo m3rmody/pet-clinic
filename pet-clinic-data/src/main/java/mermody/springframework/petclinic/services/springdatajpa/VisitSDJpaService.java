a26e91c465766689db7bf9f15cd6a0c76cae7293
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-26 16:19:04 +0200

    Implemented Spring Data JPA for Visit and added Visit on Bootstrap. Closes #47 #48.

---
+++ pet-clinic-data/src/main/java/mermody/springframework/petclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,51 @@
+package mermody.springframework.petclinic.services.springdatajpa;
+
+import mermody.springframework.petclinic.model.Visit;
+import mermody.springframework.petclinic.repositories.VisitRepository;
+import mermody.springframework.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by m3rmody on 26.09.2020
+ */
+@Service
+@Profile("pringdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-web/src/main/java/mermody/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mermody/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package mermody.springframework.petclinic.bootstrap;
 
 import mermody.springframework.petclinic.model.*;
-import mermody.springframework.petclinic.services.OwnerService;
-import mermody.springframework.petclinic.services.PetTypeService;
-import mermody.springframework.petclinic.services.SpecialtyService;
-import mermody.springframework.petclinic.services.VetService;
+import mermody.springframework.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,18 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService,
+                      VetService vetService,
+                      PetTypeService petTypeService,
+                      SpecialtyService specialtyService,
+                      VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -84,6 +87,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(dimaslPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(dimaslPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners ..... ");
 
