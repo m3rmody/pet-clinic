071dc28d21d1b9c50c99ad5ab43a57455a441ebf
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-26 15:07:47 +0200

    Created spring data jpa service for vet. Closes #41.

---
+++ pet-clinic-data/src/main/java/mermody/springframework/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,52 @@
+package mermody.springframework.petclinic.services.springdatajpa;
+
+import mermody.springframework.petclinic.model.Vet;
+import mermody.springframework.petclinic.repositories.VetRepository;
+import mermody.springframework.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by m3rmody on 26.09.2020
+ */
+
+@Service
+@Profile("pringdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
