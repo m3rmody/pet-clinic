66f7196d608f874992c0f240cf3a837d0a297204
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-26 16:01:38 +0200

    Implemented Map impl for Visit. Closes #46.

---
+++ pet-clinic-data/src/main/java/mermody/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package mermody.springframework.petclinic.services;
+
+import mermody.springframework.petclinic.model.Visit;
+
+/**
+ * Created by m3rmody on 26.09.2020
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+}
