cdd94cc31d26908f707ce69348ab72f5eb91d1e6
m3rmody <dmytro.shevchenkoit@gmail.com>
2020-09-26 15:15:25 +0200

    Created spring data jpa service for Pet. Closes #43.

---
+++ pet-clinic-data/src/main/java/mermody/springframework/petclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,51 @@
+package mermody.springframework.petclinic.services.springdatajpa;
+
+import mermody.springframework.petclinic.model.Pet;
+import mermody.springframework.petclinic.repositories.PetRepository;
+import mermody.springframework.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by m3rmody on 26.09.2020
+ */
+@Service
+@Profile("pringdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
